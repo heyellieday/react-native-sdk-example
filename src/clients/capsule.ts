4cf5251c4caf68acc4a891cf44e302a35003a718
Ellie Day <heyellieday@gmail.com>
2024-04-29 11:40:23 -0700

    my config

--- src/clients/capsule.ts
+++ src/clients/capsule.ts
@@ -1,6 +1,6 @@
 import {CapsuleMobile, Environment} from '@usecapsule/react-native-wallet';
 
 export const capsule = new CapsuleMobile(
-  Environment.BETA,
-  'd0b61c2c8865aaa2fb12886651627271',
+  Environment.SANDBOX,
+  '8ee2d015fbc6062a6e30bdc472f2946c',
 );
